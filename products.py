556a0d15c4f40af59c5b00350668e77d693d66d2
Steven Liang <gkg.yji@gmail.com>
2020-06-08 23:19:22 +0800

    add products.py

---
+++ products.py
@@ -0,0 +1,18 @@
+products = []
+while True:
+	name = input('請輸入商品名：')
+	if name == 'quit':
+		break
+	price = input('請輸入價格：')
+	# 方法一：p = []
+	#        p.append(name)
+	#        p.append(price)
+
+	p = [name, price]  # 方法二
+	products.append(p)
+	
+    # 方法三：producsts.append([name, price])
+
+print(products)
+
+print(products[0][0])
