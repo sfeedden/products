84f1d4988dbe5b46e03a8854b7aee8ee5e046830
Steven Liang <gkg.yji@gmail.com>
2020-06-11 00:00:24 +0800

    add header

--- products.py
+++ products.py
@@ -4,6 +4,7 @@ while True:
 	if name == 'quit':
 		break
 	price = input('請輸入價格：')
+	price = int(price)
 	# 方法一：p = []
 	#        p.append(name)
 	#        p.append(price)
@@ -18,4 +19,9 @@ print(products[0][0])
 
 for product in products:
 	print(product)
-	print(product[0])
+	print(product[0]) 
+
+with open('products.csv', 'w', encoding='utf-8' ) as f:
+	f.write('商品,價格\n')
+	for p in products:
+		f.write(p[0] + ',' + str(p[1]) + '\n')
